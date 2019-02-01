72757fb813992ee3f3679f8559abe99fba47994e
Oldarorn <antoine.niveau@gmail.com>
2019-02-01 09:50:20 +0100

    first commit

---
+++ config.lua
@@ -0,0 +1,33 @@
+Config                            = {}
+Config.DrawDistance               = 100.0
+Config.MarkerColor                = { r = 255, g = 210, b = 0 }
+Config.MarkerSize                 = { x = 1.5, y = 1.5, z = 1.0 }
+
+Config.Zones = {
+
+    -- WEED START --
+
+    -- HUMANE LABS COOLING START --
+
+    HumaneLabEntrance = { -- Entrance
+        Pos	= { x = 3540.5961914062, y = 3675.3364257812, z = 27.121141433716 },
+        Type = -1
+    },
+
+    HumaneLabExit = { -- Exit
+        Pos	= { x = 3540.3425292968, y = 3674.810546875, z = 19.991790771484 },
+        Type = -1
+    },
+
+    HumaneLabIn = { -- Inside
+        Pos	= { x = 3539.7551269532, y = 3671.0168457032, z = 20.99178314209 },
+        Type = -1
+    },
+
+    HumaneLabOut = { -- Outside
+        Pos	= { x = 3540.0947265625, y = 3671.203125, z = 28.121160507202 },
+        Type = -1
+    },
+
+    -- HUMANE LABS COOLING STOP --
+}
